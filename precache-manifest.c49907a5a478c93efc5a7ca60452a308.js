ccf5a3e03325ea9c4e9e766188da93bd1c13bd25
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-02-24 15:26:02 -0300

    Updates

---
+++ precache-manifest.c49907a5a478c93efc5a7ca60452a308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed461f6ed6242dea7f4d857d45a32f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "917ed21f923a1fd4f702",
+    "url": "/monsters-rolodex/static/css/main.206b6009.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917ed21f923a1fd4f702",
+    "url": "/monsters-rolodex/static/js/main.0dba65d1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
